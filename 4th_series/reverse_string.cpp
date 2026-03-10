e9e8cff9ab1c7cb82aeaf11e82ebd4423e0b6f6d
Srishti <ssrishti8968@gmail.com>
2026-03-10 21:53:56 +0530

    problem

--- 4th_series/reverse_string.cpp
+++ 4th_series/reverse_string.cpp
@@ -1,5 +1,15 @@
 #include<iostream>
+#include<string>
 using namespace std;
 int main(){
-    
+    string s = "srishti";
+    int n = s.size();
+
+    for(int i = 0; i < n/2; i++){
+        char temp = s[i];
+        s[i] = s[n-1-i];
+        s[n-1-i] = temp;
+    }
+
+    cout << s;
 }
